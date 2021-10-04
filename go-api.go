f3260579cb4af0dd7e3ecfbaa376f8bf32052699
L M Fielding <contact@lmfielding.com>
2021-10-04 15:13:59 +0100

    Add a ping route from docs.

--- go-api.go
+++ go-api.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-    fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run()
 }
